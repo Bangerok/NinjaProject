130bc1b0fb15c66fac9169c36ddb45b87f476493
Vladislav <kuznetsov.v.a.work@gmail.com>
2020-04-02 03:15:26 +0300

    0.0.3. Project build configuration. Checking Routing.

--- NinjaServer/src/main/java/bangerok/ninja/controller/TestController.java
+++ NinjaServer/src/main/java/bangerok/ninja/controller/TestController.java
@@ -4,10 +4,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/")
 public class TestController {
 
-    @RequestMapping("/")
-    public String index() {
+    @RequestMapping("test")
+    public String test() {
         return "Greetings from Spring Boot!";
     }
 }
