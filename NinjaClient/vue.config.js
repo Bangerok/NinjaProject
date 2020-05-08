ec4f2a79ad7a963aa3c5a77a971d199cb76e7a82
Vladislav Kuznetsov <kuznetsov.v.a.work@gmail.com>
2020-05-08 14:00:14 +0300

    0.1.2. VueJS dependency proceedings.

--- NinjaClient/vue.config.js
+++ NinjaClient/vue.config.js
@@ -1,18 +1,19 @@
 module.exports = {
-  outputDir: 'target/dist',
-  assetsDir: 'static',
-  publicPath: process.env.NODE_ENV === 'production' ?
-      '/ninja-server/' :
-      './',
-  devServer: {
-    contentBase: './dist',
-    compress: true,
-    port: 8000,
-    allowedHosts: [
-      'localhost:9000'
+  "outputDir": "target/dist",
+  "assetsDir": "static",
+  "publicPath": "./",
+  "devServer": {
+    "contentBase": "./dist",
+    "compress": true,
+    "port": 8000,
+    "allowedHosts": [
+      "localhost:9000"
     ],
-    proxy: 'http://localhost:9000',
-    stats: 'errors-only',
-    clientLogLevel: 'error',
+    "proxy": "http://localhost:9000",
+    "stats": "errors-only",
+    "clientLogLevel": "error"
   },
-};
+  "transpileDependencies": [
+    "vuetify"
+  ]
+}
