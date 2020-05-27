dc57cbb4d636b56bfd72dce29ee5025800ddaa7a
Vladislav <kuznetsov.v.a.work@gmail.com>
2020-05-27 14:01:09 +0300

    0.3.9. Added tests for UserRepository to test its methods.

--- NinjaServer/src/main/java/bangerok/ninja/repo/RoleRepository.java
+++ NinjaServer/src/main/java/bangerok/ninja/repo/RoleRepository.java
@@ -3,7 +3,9 @@ package bangerok.ninja.repo;
 import bangerok.ninja.domain.Role;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
 
 		Optional<Role> findByName(String name);
